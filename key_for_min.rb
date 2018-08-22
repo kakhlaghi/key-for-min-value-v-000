d61836fdff0a098904a285db29ee0fbff8078a80
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-08-22 18:46:43 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.collect { |key, value| smallest_value << value}
   smallest_value.sort { |a,b| a<=>b}
   name_hash.collect { |key, value| 
-    if value smallest_value[0] 
+    if value == smallest_value[0] 
       smallest_key << key
     end }
   
